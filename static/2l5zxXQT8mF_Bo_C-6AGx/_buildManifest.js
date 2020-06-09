bc66076d8a04a00b0d06af352326efb6c081d5fb
zhengfen <1582893052@qq.com>
2020-06-10 00:29:34 +0800

    nice-night

--- static/2l5zxXQT8mF_Bo_C-6AGx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/article":[a],"/article-list":[a],"/component":[a],"/index":[a],"/login":[a],"/photo":[a],"/release-article":[a],"/video":[a],"/videoList":[a]}}("static\u002Fchunks\u002Fbfe88ae3410ba846e6cde76744b9d72353c81777.22f4ffb3eb2df424dd3b.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
