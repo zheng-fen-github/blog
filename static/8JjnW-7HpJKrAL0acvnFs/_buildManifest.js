80c71040166c589fb72edb9b17c5eb3fccdb37c9
zhengfen <1582893052@qq.com>
2020-06-10 23:22:06 +0800

    nice-four

---
+++ static/8JjnW-7HpJKrAL0acvnFs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/article":[a],"/article-list":[a],"/component":[a],"/index":[a],"/login":[a],"/photo":[a],"/release-article":[a],"/video":[a],"/videoList":[a]}}("static\u002Fchunks\u002F7d9d828f191f18e6e737fd304db2d3debaad1525.22f4ffb3eb2df424dd3b.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
